96fd6a084ba777c59836e62aafd9791a54b08949
Amandir01 <32287790+Amandir01@users.noreply.github.com>
2017-10-09 17:09:22 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function isValidCard(arrayNumbers) {
+
+  var numbers = parseInt(prompt('Ingresa tu número de trajeta'));
+  arrayNumbers = [numbers]
+
+  return console.log(Numbers)
+
+}
+
+isValidCard()
